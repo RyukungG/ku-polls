39cc3b41827723c0515d5698700af201adc0dbd9
RyukungG <ryu.nosuke@hotmail.com>
2022-09-11 13:57:32 +0700

    add end_date and Add two methods is_published and can_vote

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,31 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date expired', null=True, default=None)
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
-        now = timezone.now()
+        now = timezone.localtime()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.localtime()
+        return self.pub_date <= now
+
+    def can_vote(self):
+        if self.end_date is None:
+            return self.is_published()
+        return self.pub_date <= timezone.localtime() <= self.end_date
 
     def __str__(self):
         return self.question_text
@@ -22,5 +38,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
